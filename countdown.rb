d933016718ededc1e81dd0b9ecf8f90470c9ae83
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-11 04:09:56 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,10 @@
 #write your code here
 
-  def countdown
+def countdown
   number = 10
   while number > 0
        puts "#{number} SECONDS(S)!"
        number -= 1
   end
-  
   return "HAPPY NEW YEAR!"
 end
