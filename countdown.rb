b769b7456090ace2b0c12a9a3046b284d35a5b14
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-11 04:12:28 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,17 @@
 #write your code here
 
-def countdown
-  number = 10
-  while number > 0
-       puts "#{number} SECONDS(S)!"
-       number -= 1
+  def countdown(count)
+    while count >0
+      puts "#{count} SECOND(S)!"
+      count-=1
+    end
+    "HAPPY NEW YEAR!"
   end
-  return "HAPPY NEW YEAR!"
-end
+  
+  def countdown_with_sleep(count)
+    while count > 0
+      count-=1
+      sleep(1)
+    end
+      puts "0 left. HAPPY NEW YEAR!"
+  end
